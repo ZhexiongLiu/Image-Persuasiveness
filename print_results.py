d916b6822ad630bfecbf25689f439fc7a258f381
zhexiongliu <macodreu@gmail.com>
2022-07-17 15:48:23 -0700

    updates

--- print_results.py
+++ print_results.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 
-def report_res(subdir, exp_name, is_majority=False):
+def report_res(subdir, exp_name, is_majority=False, print_option=1):
     f1_list = []
     precision_list = []
     recall_list = []
@@ -52,15 +52,18 @@ def report_res(subdir, exp_name, is_majority=False):
         acc_list.append(fold_acc)
         auc_list.append(fold_auc)
 
-    # m_f1, m_precision, m_recall, m_acc, m_auc = np.round(np.mean(f1_list),3), np.round(np.mean(precision_list),3), np.round(np.mean(recall_list),3), np.round(np.mean(acc_list),3), np.round(np.mean(auc_list),3)
-    # std_f1, std_precision, std_recall, std_acc, std_auc = np.round(np.std(f1_list),2), np.round(np.std(precision_list),2), np.round(np.std(recall_list),2), np.round(np.std(acc_list),2), np.round(np.std(auc_list),2)
-    # res_str = f"{exp_name: <30}{m_precision : <10}{m_recall : <10}{m_f1 : <10}{m_acc : <10}"
-    # res_str = f"{m_precision}({std_precision}) {m_recall}({std_recall}) {m_f1}({std_f1}) {m_auc}({std_auc}) {m_acc}({std_acc})"
+    if print_option == 0:
+        m_f1, m_precision, m_recall, m_acc, m_auc = np.round(np.mean(f1_list),3), np.round(np.mean(precision_list),3), np.round(np.mean(recall_list),3), np.round(np.mean(acc_list),3), np.round(np.mean(auc_list),3)
+        std_f1, std_precision, std_recall, std_acc, std_auc = np.round(np.std(f1_list),2), np.round(np.std(precision_list),2), np.round(np.std(recall_list),2), np.round(np.std(acc_list),2), np.round(np.std(auc_list),2)
+        # res_str = f"{exp_name: <30}{m_precision : <10}{m_recall : <10}{m_f1 : <10}{m_acc : <10}"
+        # res_str = f"{m_precision}({std_precision}) {m_recall}({std_recall}) {m_f1}({std_f1}) {m_auc}({std_auc}) {m_acc}({std_acc})"
+        res_str = f"${m_precision}_(\pm{std_precision})$ ${m_recall}_(\pm{std_recall})$ ${m_f1}_(\pm{std_f1})$ ${m_auc}_(\pm{std_auc})$ ${m_acc}_(\pm{std_acc})$"
 
-    m_f1, m_precision, m_recall, m_acc, m_auc = np.round(np.mean(f1_list)*100,2), np.round(np.mean(precision_list)*100,2), np.round(np.mean(recall_list)*100,2), np.round(np.mean(acc_list)*100,2), np.round(np.mean(auc_list)*100,2)
-    std_f1, std_precision, std_recall, std_acc, std_auc = np.round(np.std(f1_list)*100,2), np.round(np.std(precision_list)*100,2), np.round(np.std(recall_list)*100,2), np.round(np.std(acc_list)*100,2), np.round(np.std(auc_list)*100,2)
-    res_str = f"{m_precision} {m_recall} {m_f1} {m_auc} {m_acc}"
+    else:
+        m_f1, m_precision, m_recall, m_acc, m_auc = np.round(np.mean(f1_list)*100,2), np.round(np.mean(precision_list)*100,2), np.round(np.mean(recall_list)*100,2), np.round(np.mean(acc_list)*100,2), np.round(np.mean(auc_list)*100,2)
+        std_f1, std_precision, std_recall, std_acc, std_auc = np.round(np.std(f1_list)*100,2), np.round(np.std(precision_list)*100,2), np.round(np.std(recall_list)*100,2), np.round(np.std(acc_list)*100,2), np.round(np.std(auc_list)*100,2)
 
+        res_str = f"{m_precision} {m_recall} {m_f1} {m_auc} {m_acc}"
 
     print(f"{exp_name} 5 fold validation...")
     print(f"{'precision' : <10}{'recall' : <10}{'f1' : <10}{'auc' : <10}{'acc' : <10}")
@@ -79,13 +82,13 @@ def get_res_table():
         # with open(os.path.join(subdir, "report.txt"), "r") as f:
         #     lines = f.readlines()
         # exp_res = f"{exp_name}{'': <10}{lines[2][:-1]}"
-        exp_res = report_res(subdir, exp_name)
-        exp_majority_res = report_res(subdir, exp_name, is_majority=True)
+        exp_res = report_res(subdir, exp_name, is_majority=False, print_option=0)
+        exp_majority_res = report_res(subdir, exp_name, is_majority=True, print_option=0)
 
         exp_res = exp_res.split(" ")
         exp_majority_res = exp_majority_res.split(" ")
-        res_str = f"{exp_name} & {exp_res[0]} & {exp_res[1]} & {exp_res[2]} & {exp_res[3]} & {exp_res[4]}"
-        res_majority_str = f"{exp_name} & {exp_majority_res[0]} & {exp_majority_res[1]} & {exp_majority_res[2]} & {exp_majority_res[3]} & {exp_majority_res[4]}"
+        res_str = f"{exp_name} & {exp_res[0]} & {exp_res[1]} & {exp_res[2]} & {exp_res[3]}"
+        res_majority_str = f"{exp_name} & {exp_majority_res[0]} & {exp_majority_res[1]} & {exp_majority_res[2]} & {exp_majority_res[3]}"
         exp_res_lsit.append(res_str)
         exp_res_majority_lsit.append(res_majority_str)
 
@@ -115,4 +118,4 @@ def get_data_stas():
 
 
 get_res_table()
-# get_data_stas()
+get_data_stas()
